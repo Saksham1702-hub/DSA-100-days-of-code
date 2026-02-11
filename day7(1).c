14efb5812940e9665bc2d7fef16193b2aa93596e
Saksham1702-hub <sakshamyadav4142@gmail.com>
2026-02-11 19:47:39 +0530

    Fibonacci term at nth position

---
+++ day7(1).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Recursive function
+int fib(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    int result = fib(n);
+
+    printf("Fibonacci number at position %d is: %d\n", n, result);
+
+    return 0;
+}
